d66d7b6312a7e14dbe3cc43276653a34f559168e
Vladyslav Popov <vladpopov90@gmail.com>
2017-11-22 18:49:11 +0200

    Delete 1_run_webHook.sh

--- 1_run_webHook.sh
+++
@@ -1,4 +0,0 @@
-curl \
-  -F "url=https://bbstore.ru/main.php" \
-    -F "certificate=///etc/nginx/ssl/nginx.crt" \
-      "https://api.telegram.org/bot496323376:AAG8ZWlXI01v1pai_IPYmYnSfVAdm6Y78nc/setwebhook"
